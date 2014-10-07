bda61a1f7aff326485fbd4235675b81ff390a821
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2014-10-07 08:46:19 +0300

    Page 51 - Install Gruntfile+extensions, testing and external software

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
-var handlebars = require('express-handlebars')
+var handlebars = require('express-handlebars');
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -12,8 +12,14 @@ app.listen(app.get('port'), function(){
 		app.get('port') + '; press Ctrl-C to terminate.' );
 });
 
-
 var fortune = require('./lib/fortune.js');
+
+
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' &&
+	req.query.test === '1';
+	next();
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -23,8 +29,25 @@ res.render("home");
 });
 
 app.get('/about', function(req, res){
-//res.type('text/plain');
-res.render('about', { fortune: fortune.getFortune() })
+	//res.type('text/plain');
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	 });
+});
+
+//tours folder :
+
+app.get('/tours/hood-river', function(req, res){
+res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function(req, res){
+res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+res.render('tours/request-group-rate');
 });
 
 // custom 404 page
